ff82bd2c6dfba4af0c292590b042a8f0ba07bc37
Jay Jeyaruban <jay@jayjeyaruban.com>
2018-02-25 00:16:59 +0000

    Added comments to client interface for clarity

--- src/main/java/comp3013/group3/ebayscraper/httpclient/Client.java
+++ src/main/java/comp3013/group3/ebayscraper/httpclient/Client.java
@@ -6,10 +6,26 @@ import java.util.Properties;
 
 public interface Client {
 
+	/**
+	 * Wrapper around the getItem eBay API
+	 * @param itemId eBay item ID to be queried
+	 * @return JSONObject
+	 */
 	JSONObject getItem(String itemId);
 
+	/**
+	 * TODO
+	 * Will currently return null
+	 * @param searchTerm Search term to be queried
+	 * @return JSONObject
+	 */
 	JSONObject search(String searchTerm);
 
+	/**
+	 * Builder for eBay Client interface. See ClientTest for example instantiation
+	 * @param properties Should contain config information. E.g. urls, clientID, etc.
+	 * @return Client
+	 */
 	static Client builder(Properties properties) {
 		return new EbayClient(properties);
 	}
